9481ddff5f79d244f95c6c33caffe169c084084d
Salimov-dev <salimov.rent@mail.ru>
2023-11-28 21:35:05 +0300

    фронт настроен

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" exact element={<Join />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  );
+}
+
+export default App;
